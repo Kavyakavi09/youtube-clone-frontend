9e1c52cfecb4c088b242d7fc56ad66eb2ef1b6cc
Kavya K <95955787+Kavyakavi09@users.noreply.github.com>
2022-08-23 14:06:41 +0530

    changed layout

--- src/App.js
+++ src/App.js
@@ -1,47 +1,28 @@
-import Menu from './components/Menu/Menu';
-import { useState } from 'react';
-import Navbar from './components/Navbar/Navbar';
-import { Container, Main, Wrapper } from './style';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
-import { darkTheme, lightTheme } from './utils/Theme';
 import Home from './pages/Home/Home';
 import SignIn from './pages/SignIn/SignIn';
 import Video from './pages/Video/Video';
 import Search from './pages/Search/Search';
 import { useSelector } from 'react-redux';
+import SharedLayout from './pages/SharedLayout';
 
 function App() {
-  const [darkMode, setDarkMode] = useState(true);
   const { currentUser } = useSelector((state) => state.user);
   return (
-    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-      <Container>
-        <BrowserRouter>
-          <Menu setDarkMode={setDarkMode} darkMode={darkMode} />
-          <Main>
-            <Navbar />
-            <Wrapper>
-              <Routes>
-                <Route path='/'>
-                  <Route index element={<Home type='random' />} />
-                  <Route path='trends' element={<Home type='trend' />} />
-                  <Route path='subscriptions' element={<Home type='sub' />} />
-                  <Route path='search' element={<Search />} />
-                  <Route
-                    path='signin'
-                    element={currentUser ? <Home /> : <SignIn />}
-                  />
-                  <Route path='video'>
-                    <Route path=':id' element={<Video />} />
-                  </Route>
-                </Route>
-              </Routes>
-            </Wrapper>
-          </Main>
-        </BrowserRouter>
-      </Container>
-    </ThemeProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/signin' element={currentUser ? <Home /> : <SignIn />} />
+        <Route path='/' element={<SharedLayout />}>
+          <Route index element={<Home type='random' />} />
+          <Route path='trends' element={<Home type='trend' />} />
+          <Route path='subscriptions' element={<Home type='sub' />} />
+          <Route path='search' element={<Search />} />
+          <Route path='video'>
+            <Route path=':id' element={<Video />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/SignIn/style.js
+++ src/pages/SignIn/style.js
@@ -1,22 +1,22 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  padding: 60px 60px;
+  padding: 20px 20px;
   margin-left: 20px;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: calc(100%-56%)
-  color: ${({ theme }) => theme.text};
+  height: calc(100%-56%);
+  color: white;
 `;
 
 export const Wrapper = styled.div`
   display: flex;
   align-items: center;
   flex-direction: column;
-  background-color: ${({ theme }) => theme.bgLighter};
-  border: 1px solid ${({ theme }) => theme.soft};
+  background-color: #202020;
+  border: 1px solid #373737;
   padding: 20px 50px;
   gap: 10px;
 `;
@@ -31,12 +31,12 @@ export const SubTitle = styled.h2`
 `;
 
 export const Input = styled.input`
-  border: 1px solid ${({ theme }) => theme.soft};
+  border: 1px solid #373737;
   border-radius: 3px;
   padding: 10px;
   background-color: transparent;
   width: 100%;
-  color: ${({ theme }) => theme.text};
+  color: white;
 `;
 
 export const Button = styled.button`
@@ -45,15 +45,15 @@ export const Button = styled.button`
   padding: 10px 20px;
   font-weight: 500;
   cursor: pointer;
-  background-color: ${({ theme }) => theme.soft};
-  color: ${({ theme }) => theme.textSoft};
+  background-color: #373737;
+  color: #aaaaaa;
 `;
 
 export const More = styled.div`
   display: flex;
   margin-top: 10px;
   font-size: 12px;
-  color: ${({ theme }) => theme.textSoft};
+  color: #aaaaaa;
 `;
 
 export const Links = styled.div`
